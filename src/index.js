581a0af4f93ee3cfc8f152bae2b7575109ae89c1
Alex Ilchenko <alex.ilchenko@blueprint-london.com>
2019-02-26 13:26:09 +0000

    react

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => <div>Wow</div>
+
+const root = document.createElement('root')
+document.body.appendChild(root)
+
+ReactDOM.render(<App />, root)
