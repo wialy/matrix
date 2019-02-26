7c71c446fcaeb40b1bce6d7c2acdeb11fa09afec
Alex Ilchenko <alex.ilchenko@blueprint-london.com>
2019-02-26 12:49:52 +0000

    basic webpack configuration

---
+++ webpack.common.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Matrix'
+    })
+  ]
+}
